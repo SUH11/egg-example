fc2502f423d424aaaa36282f36bb843ae7ba4c80
suhong.huang <1067489058@qq.com>
2019-08-05 19:45:12 +0800

    修改reme

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ see [egg docs][egg] for more detail.
 
 ### Development
 
+运行
+
+1. 安装redis并启动
+关于redis的安装与运行可以参考[这里](https://github.com/antbaobao/AntBlog/issues/42)
+2. 安装mongodb并启动
+3. 开发
+
 ```bash
 $ npm i
 $ npm run dev
---
+++ app/model/wxUser.js
@@ -0,0 +1,14 @@
+import { ObjectId } from 'bson';
+
+module.export = app => {
+    const mongoose = app.mongoose;
+    const Schema = mongoose.Schema;
+
+    // const WxUserSchema = new Schema({
+    //     '_id': Schema.Types.ObjectId,
+    //     '_openid': {
+    //         type: Schema.Types.ObjectId
+    //     },
+        
+    // })
+}
